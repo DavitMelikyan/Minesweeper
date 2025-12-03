3e26cf17e091cda4d9f15191f96e2d4699b43667
Davit Melikyan <davidmelikyan900@gmail.com>
2025-12-04 01:07:45 +0400

    Add StatusPanel class in MainWindow.hpp

--- include/mainwindow.hpp
+++ include/mainwindow.hpp
@@ -2,13 +2,60 @@
 #define MAINWINDOW_HPP
 
 #include <QMainWindow>
+#include <QWidget>
+#include <QApplication>
+#include <QPushButton>
+#include <QLabel>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QTimer>
+#include <QLCDNumber>
+#include <QMenuBar>
+
+enum class GameState { Playing, Won, Lost };
+
+class StatusPanel : public QWidget {
+    Q_OBJECT
+private:
+    QLCDNumber* mineCounter;
+    QPushButton* restart;
+    QLCDNumber* timerCounter;
+    QLabel* diffLabel;
+
+    void setUI();
+    void setConnections();
+public:
+    StatusPanel(QWidget* parent = nullptr);
+    void setFaceState(GameState state);
+    void changeDiff(const QString& diff);
+    void updateMineCount(int mcount);
+    void updateTimer(int nseconds);
+};
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
+private:
+    StatusPanel* statusPanel;
+    QMenuBar* menuBar;
+    QPushButton* backToMenu;
+    QMenu* menu;
+    QAction* startNewGame;
+    QAction* changeGameDiff;
+    QAction* exitGame;
+    int m_rows;
+    int m_cols;
+    int m_mines;
 
+    void setUI();
+    void createStatusPanel();
+    void setConnections();
+private slots:
+    void newGame();
+    void changeDifficulty();
+    void exit();
+    void restartGame();
 public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+    MainWindow(int rows, int cols, int mines, QWidget* parent = nullptr);
 };
 #endif // MAINWINDOW_HPP
